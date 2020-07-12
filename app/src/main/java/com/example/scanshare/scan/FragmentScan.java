bf9c9c95268dfebb3bc63f44ba7e2dd00b2cbf04
vivek jangir <vivekjangir2321@gmail.com>
2020-07-12 21:28:22 +0545

    scan complete

--- app/src/main/java/com/example/scanshare/scan/FragmentScan.java
+++ app/src/main/java/com/example/scanshare/scan/FragmentScan.java
@@ -1,31 +1,123 @@
 package com.example.scanshare.scan;
 
-import android.os.Bundle;
+import android.Manifest;
+import android.app.Activity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.scanshare.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
+
+import java.io.IOException;
 
 
 public class FragmentScan extends Fragment {
+    private int REQUEST_PERMISSIONS = 1;
+    private static final int REQUEST_CODE = 99;
+
+    ImageView scannedImageView;
+    boolean boolean_permission;
+
+    String[] PERMISSIONS = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
 
         FloatingActionButton scan = view.findViewById(R.id.scan);
+        scannedImageView = view.findViewById(R.id.scannedImage);
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if ((ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                        (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)) {
+                    openCamera();
+                } else {
+                    fn_permission();
+                }
             }
         });
 
         return view;
     }
+
+    private void fn_permission() {
+        if ((ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) )  {
+
+            requestPermissions( PERMISSIONS, 1);
+
+        } else {
+            boolean_permission = true;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSIONS)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+                Toast.makeText(getContext(), "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void openGallery() {
+        int preference = ScanConstants.OPEN_MEDIA;
+        Intent intent = new Intent(getContext(), ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    public void openCamera() {
+        int preference = ScanConstants.OPEN_CAMERA;
+        Intent intent = new Intent(getContext(), ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == REQUEST_CODE) {
+                Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                    getActivity().getContentResolver().delete(uri, null, null);
+                    scannedImageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
