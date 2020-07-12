2f7e3c2bc5f25a76fb59e03d29b60cd9ea36e00b
17BTCS023 <manisha.17cs@cmr.edu.in>
2020-07-12 15:31:31 +0530

    added files for Login Signup

---
+++ app/src/main/java/com/example/scanshare/LoginSignup/Signup.java
@@ -0,0 +1,16 @@
+package com.example.scanshare.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.scanshare.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
